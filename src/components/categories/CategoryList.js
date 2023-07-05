2703390e9db7b9ca761aeeb2efb9dd5cc17c35da
tugrulyuce <tugrulyuce80@gmail.com>
2023-07-05 15:50:02 +0300

    Navi

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class CategoryList extends Component {
+  render() {
+    return (
+      <div>CategoryList</div>
+    )
+  }
+}
+
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./DashBoard";
 
 function App() {
   return (
-    <div >
-      <h1>App</h1>
-    </div>
+    <Container>
+      <Navi />
+      <Dashboard/>
+   </Container>
   );
 }
 
