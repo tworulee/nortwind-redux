41091388e6199fb16fb8762882654ea2fa29edf8
tugrulyuce <tugrulyuce80@gmail.com>
2023-07-06 15:04:55 +0300

    redux store config

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux"
+import changeCategoryReducers from "./changeCategoryReducers"
+
+const rootReducer = combineReducers({
+    changeCategoryReducers
+})
+
+export default rootReducer
